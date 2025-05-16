5117eac554a69ee8f5f54296b4ea4a2ee15e48b8
Narumi420 <narumicabrerapiscoya@gmail.com>
2025-05-16 03:08:46 -0500

    Parte de productos lista

--- api/serializers.py
+++ api/serializers.py
@@ -2,10 +2,20 @@
 from rest_framework import serializers
 from .models import Especialidad
 from .choices import Estado
-
+from .models import Producto
 class EspecialidadSerializer(serializers.ModelSerializer):
     estado = serializers.ChoiceField(choices=Estado.ESTADO_CHOICES, required=False, allow_null=True)
 
     class Meta:
         model = Especialidad
         fields = ['id', 'nombre', 'estado']
+
+
+class ProductoSerializer(serializers.ModelSerializer):
+    estado = serializers.ChoiceField(choices=Estado.ESTADO_CHOICES, required=False, allow_null=True)
+
+    class Meta:
+        model = Producto
+        fields = ['id', 'nombre', 'descripcion', 'proveedor', 
+                  'tipo', 'subtipo','stock','precio_venta','fecha_vencimiento','estado']
+        
--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import EspecialidadViewSet
+from .views import *
 
 router = DefaultRouter()
 router.register(r'especialidades', EspecialidadViewSet)
-
-urlpatterns = [
-    path('api/', include(router.urls)),
-]
+router.register(r'productos', ProductoViewSet) 
+urlpatterns = router.urls
